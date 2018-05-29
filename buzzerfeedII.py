85c290fe31838aff6dee85d46d11374c0ce2f180
marc mandela <lelambonzo@gmail.com>
2018-05-29 18:58:18 +0300

    Update buzzerfeedII.py

--- buzzerfeedII.py
+++ buzzerfeedII.py
@@ -21,7 +21,7 @@ def destroy():
 	GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
-	print 'Press Ctrl+C to end the program...'
+	print ("Press Ctrl+C to end the program...")
 	setup()
 	try:
 		loop()
